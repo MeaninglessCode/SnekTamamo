814cba901bee01c7a5fcff31e2fe5384e0ee386e
MeaninglessCode <derek0taylor@gmail.com>
2017-12-01 23:47:48 -0600

    Currently working on a rewrite.

---
+++ TamamoSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TamamoSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
