2d4e6f56db1533258acc4f1371cff56505ec7b52
MeaninglessCode <derek0taylor@gmail.com>
2017-12-02 11:36:44 -0600

    Rewrite in progress.

--- TamamoSharp/Program.cs
+++ TamamoSharp/Program.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using TamamoSharp.Services;
+using Discord.Commands;
 
 namespace TamamoSharp
 {
     class Program
     {
+        private DiscordSocketClient _client;
+        private IConfiguration _cfg;
+
         static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+            _cfg = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json")
+                .Build();
+
+            IServiceProvider svc = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_cfg)
+                .AddSingleton(new CommandService(new CommandServiceConfig
+                {
+                    DefaultRunMode = RunMode.Async,
+                    LogLevel = LogSeverity.Verbose
+                }))
+                .AddSingleton<CommandHandler>()
+                .AddSingleton<Start>()
+                .AddLogging()
+                .BuildServiceProvider();
+
+            await svc.GetRequiredService<Start>().StartAsync();
+            await svc.GetRequiredService<CommandHandler>().InitializeAsync(svc);
+
+            await Task.Delay(-1);
         }
     }
 }
---
+++ TamamoSharp/Utils/Services/CommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using Discord;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace TamamoSharp.Services
+{
+    class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _cmdsvc;
+        private readonly IConfiguration _cfg;
+        private IServiceProvider _svcprovider;
+        private int argPos = 0;
+
+        public CommandHandler(IServiceProvider svcprovider, CommandService cmdsvc, DiscordSocketClient client, IConfiguration cfg)
+        {
+            _cfg = cfg;
+            _cmdsvc = cmdsvc;
+            _client = client;
+            _svcprovider = svcprovider;
+
+            _client.MessageReceived += MessageReceived;
+        }
+
+        public async Task InitializeAsync(IServiceProvider provider)
+        {
+            _svcprovider = provider;
+            await _cmdsvc.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        public async Task MessageReceived(SocketMessage sMsg)
+        {
+            if (!(sMsg is SocketUserMessage msg) || (msg.Source != MessageSource.User))
+                return;
+            else if (msg.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                SocketCommandContext ctx = new SocketCommandContext(_client, msg);
+                IResult result = await _cmdsvc.ExecuteAsync(ctx, argPos, _svcprovider);
+
+                if (result.Error.HasValue && (result.Error.Value != CommandError.UnknownCommand))
+                    await ctx.Channel.SendMessageAsync(result.ToString());
+                else if (result.Error.HasValue)
+                    await ctx.Channel.SendMessageAsync(result.ToString());
+            }
+        }
+    }
+}
